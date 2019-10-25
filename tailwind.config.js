5e1ed7c3cc534691cb64d0654289462ddf65a965
Roni Estein <roni.estein@gmail.com>
2019-10-25 18:44:44 -0500

    Setup Tailwind config to match project in case that has an effect and cleared wilcome view

---
+++ tailwind.config.js
@@ -0,0 +1,125 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    customForms: theme => ({
+      default: {
+        'input, textarea, multiselect, select': {
+          borderRadius: theme('borderRadius.none'),
+          borderColor: theme('colors.gray.400'),
+          borderWidth: theme('width.px'),
+          fontSize: theme('fontSize.sm'),
+          boxShadow: theme('boxShadow.md'),
+          paddingTop: theme('padding.3'),
+          paddingBottom: theme('padding.3'),
+        },
+        checkbox: {
+          height: theme('height.5'),
+          width: theme('width.5'),
+          boxShadow: theme('boxShadow.md'),
+          color: theme('colors.indigo.800'),
+        },
+        radio: {
+          boxShadow: theme('boxShadow.md'),
+          height: theme('height.5'),
+          width: theme('width.5'),
+          color: theme('colors.indigo.800'),
+          icon: '<svg fill="#fff" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="4.5"/></svg>'
+        },
+
+      },
+      required: {
+        'input, textarea, multiselect, select': {
+          borderRadius: theme('borderRadius.none'),
+          borderColor: theme('colors.gray.600'),
+          borderWidth: theme('width.px'),
+          fontSize: theme('fontSize.sm'),
+          boxShadow: theme('boxShadow.md'),
+          paddingTop: theme('padding.3'),
+          paddingBottom: theme('padding.3'),
+          paddingLeft: theme('padding.3'),
+
+        },
+      },
+    }),
+    screens: {
+      xs: '440px',
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    fontSize: {
+      'xx': '0.550rem',
+      'xs': '.625rem',     // 10px
+      'sm': '.75rem',     // 12px
+      'base': '.875rem',    // 14px
+      'lg': '1rem',     // 16px
+      'xl': '1.125rem',   // 18px
+      '2xl': '1.25rem',    // 20px
+      '3xl': '1.5rem',    // 24px
+      '4xl': '1.75rem',  // 28px
+      '5xl': '2.25rem',   // 36px
+      '6xl': '3rem',      // 48px
+      '7xl': '4rem',      // 48px
+    },
+
+    extend: {
+      fontFamily: {
+        sans: [
+          'Avenir Next',
+          'Proxima Nova',
+          ...defaultTheme.fontFamily.sans,
+        ],
+        header: [
+            'Montserrat',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+      colors: {
+        smoke: {
+          '100': 'rgba(0, 0, 0, 0.1)',
+          '200': 'rgba(0, 0, 0, 0.2)',
+          '300': 'rgba(0, 0, 0, 0.3)',
+          '400': 'rgba(0, 0, 0, 0.4)',
+          '500': 'rgba(0, 0, 0, 0.5)',
+          '600': 'rgba(0, 0, 0, 0.6)',
+          '700': 'rgba(0, 0, 0, 0.7)',
+          '800': 'rgba(0, 0, 0, 0.8)',
+          '900': 'rgba(0, 0, 0, 0.9)',
+        },
+        turquoise:{
+          '100': '#BDDBED',
+          '200': '#A2E1F3',
+          '300': '#80d6ef',
+          '400': '#71c2f5',
+          '500': '#1899be',
+          '600': '#0E7BBF',
+          '700': '#094d78',
+          '800': '#041f30',
+          '900': '#03111B',
+        },
+        'white-alpha': 'hsla(0, 0%, 100%, 0.15)',
+        'white-semi-trans': 'hsla(0, 0%, 100%, 0.65)',
+      },
+      width: {
+        '7': '1.75rem',
+        '14': '3.5rem',
+      },
+      height: {
+        '7': '1.75rem',
+        '80': '20rem',
+        '96': '24rem',
+        '128': '32rem',
+        '192': '48rem',
+        '256': '64rem',
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ]
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,31 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+require('laravel-mix-purgecss')
+let tailwindcss = require('tailwindcss')
+let postcssImport = require('postcss-import')
+let postcssNesting = require('postcss-nesting')
+let postcssPresetEnv = require('postcss-preset-env')
+let selectors = require('postcss-custom-selectors')
+let colorMod = require('postcss-color-mod-function')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+function isInWatch(){
+    return process.argv.includes('--watch')
+}
+
+function isHot(){
+    return process.argv.includes('--hot')
+}
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        postcssPresetEnv(),
+        postcssImport(),
+        postcssNesting(),
+        selectors(),
+        colorMod(),
+        tailwindcss('tailwind.config.js'),
+    ])
+    .purgeCss()
+if(!isHot()){
+    mix.version()
+        .browserSync('livewire-bug.test')
+}
